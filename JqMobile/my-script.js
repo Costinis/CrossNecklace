91ddf0eb4631d9fcf61c38a5a21ef4a55b907d0d
Costinis <nistor_costi@yahoo.com>
2013-10-11 11:30:57 +0300

    up14

--- JqMobile/my-script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-  $(document).ready(function(){ 
-     var myPhotoSwipe = $(".gallery a").photoSwipe({ 
-     enableMouseWheel: false ,
-     enableKeyboard: false ,
-	 backButtonHideEnabled: false,
-     captionAndToolbarAutoHideDelay: 0
-    }); 
-  });
