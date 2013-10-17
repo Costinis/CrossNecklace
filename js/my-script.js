bec10550bb9c8371c5c4758bee47054878c4382e
Costinis <nistor_costi@yahoo.com>
2013-10-17 14:34:01 +0300

    op4

---
+++ js/my-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.btnLeft').html('Prev');
+	$('.btnRight').html('Next');
+	$('.nr').html('120');
+	$('.titlequest').html('<img src="images/question.png" width="25"><p>Question:</p>');
+	$('.titleans').html('<img src="images/answer.png" width="25"><p>Answer:</p>');
+	$('.header h1').html('Q & A');
+});
