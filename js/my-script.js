f2f0c922bd4f1df0b3095c6b629775a0527c26ff
Costinis <nistor_costi@yahoo.com>
2013-10-19 12:26:58 +0300

    op8

--- js/my-script.js
+++ js/my-script.js
@@ -1,11 +1,3 @@
-$(document).ready(function() {
-    $('.btnLeft').html('Prev');
-	$('.btnRight').html('Next');
-	$('.sum').html('120');
-	$('.titlequest').html('<img src="images/question.png" width="25"><p>Question:</p>');
-	$('.titleans').html('<img src="images/answer.png" width="25"><p>Answer:</p>');
-	$('.header h1').html('Q & A');
-});
 
 $(function() {
 
