6a07cde0a753db3379ebf3356aae21828e647274
Costinis <nistor_costi@yahoo.com>
2013-10-19 11:05:22 +0300

    op5

--- js/my-script.js
+++ js/my-script.js
@@ -1,8 +1,44 @@
 $(document).ready(function() {
     $('.btnLeft').html('Prev');
 	$('.btnRight').html('Next');
-	$('.nr').html('120');
+	$('.sum').html('120');
 	$('.titlequest').html('<img src="images/question.png" width="25"><p>Question:</p>');
 	$('.titleans').html('<img src="images/answer.png" width="25"><p>Answer:</p>');
 	$('.header h1').html('Q & A');
 });
+
+$(function() {
+
+	$.getJSON('data2.json', function(data) {
+		var template = $('#speak').html();
+		var html = Mustache.to_html(template, data);
+		$('.container1').html(html);		
+		
+		$('.container1').cycle({
+			fx: 'shuffle',
+			pause: 1,
+			next: '.btnRight',
+			prev: '.btnLeft',
+			speed: 500,
+			timeout: 0
+		});
+		
+	}); //getJSON
+	
+	$.getJSON('number.json', function(data) {
+		var template = $('#count').html();
+		var html = Mustache.to_html(template, data);
+		$('.nr').html(html);		
+		
+		$('.nr').cycle({
+			fx: 'fade',
+			pause: 1,
+			next: '.btnRight',
+			prev: '.btnLeft',
+			speed: 500,
+			timeout: 0
+		});
+		
+	}); //getJSON
+	
+}); //function
